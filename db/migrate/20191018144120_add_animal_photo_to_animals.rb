c2600054cfa8843d6e51763745e9b3cce5e8b703
cdwhitt <cwhitt91@gmail.com>
2019-10-18 13:57:33 -0400

    Configuration for AWS S3 file storage with carrierwave to upload photos

---
+++ db/migrate/20191018144120_add_animal_photo_to_animals.rb
@@ -0,0 +1,8 @@
+class AddAnimalPhotoToAnimals < ActiveRecord::Migration[5.2]
+  def up
+    add_column :animals, :photo, :string, null: false
+  end
+  def down
+    remove_column :animals, :photo
+  end
+end
---
+++ spec/features/profile_photo_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'profile photo' do
+  scenario 'user uploads a profile photo' do
+    visit root_path
+    click_link "Sign Up"
+
+    fill_in "Email", with: "ash@s-mart.com"
+    fill_in "Password", with: "abc123"
+    fill_in "Password confirmation", with: "abc123"
+    attach_file :user_profile_photo, "#{Rails.root}/spec/support/images/photo.png"
+    click_button "Sign up"
+
+    expect(page).to have_content("Welcome! Thanks for joining")
+    expect(page).to have_css("img[src*='photo.png']")
+  end
+end
